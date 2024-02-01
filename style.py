b0c1cc0e34a7ca55e1470f149a3ff6e0ebe6d1d5
TaniaRaj1010 <109296288+TaniaRaj1010@users.noreply.github.com>
2024-02-01 09:29:40 -0500

    Add files via upload

---
+++ style.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+font_size=16
+font="Arial"
+color_arr=['#FFE4E1','#CD5C5C','#CD2626','#BFEFFF','#00BFFF','#104E8B']
+sys_nms=["pkg","pka","pkl","pkkg","pkka","pkkl"]
+sys_labels=["(kG)₁₅+(eG)₁₅ ",'(kA)₁₅ +(eA)₁₅','(kL)₁₅ +(eL)₁₅ ','(kKg)₅ +(eEgEeG)₅','(kKaKkA)₅ +(eEaEeA)₅','(kKlKkL)₅+(eElEeL)₅']
+packed_strands=["AA","AB","AC","AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "OA", "OB", "OC", "OD", "OE", "OF", "OG", "OH", "OI", "OJ", "OK", "OL", "OM", "ON", "OO", "OP", "OQ", "OR", "OS", "OT", "OU", "OV", "OW", "OX", "OY", "OZ"]
+
+ss_colors=["#09486b","#045785","#006ca7","#ffac80","#ff7226","#00e64d","#ffff00","#800080"]
